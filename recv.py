3460c5fa1decfc3267b3e5bc366b26d03a52dc99
Param Panwar <85060442+parampanwar36@users.noreply.github.com>
2023-11-06 21:05:59 +0530

    recv.py

--- recv.py
+++ recv.py
@@ -1,21 +1,32 @@
 import socket
+import threading
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_ip = "127.0.0.1"
-server_port = 9999
-s.connect((server_ip, server_port))
+my_ip = "127.0.0.1"
+my_port = 9999
+my_address = (my_ip, my_port)
+s.bind(my_address)
+print("Welcome to UDP chatroom!")
+clients = {}
+connected = False  # To keep track of whether a client is connected
 
-print("Welcome to the Multi-User Chatroom")
+def receive_messages():
+    global connected  # Use the global variable
+    while True:
+        data, client_address = s.recvfrom(100)
+        new_data = data
+        received_msg = new_data.decode('ascii')
+        print("Received Message:", received_msg)
 
-while True:
-    msg = input("Please enter your message: ")
+        if not connected:
+            print(f"Client {client_address} connected.")
+            connected = True
 
+        # Get a reply message from the user
+        reply_msg = input("Reply: ")
 
-    new_msg = msg.encode('ascii')
-    s.send(new_msg)
+        # Encode and send the reply message back to the sender
+        s.sendto(reply_msg.encode('ascii'), client_address)
 
-    response = s.recv(100)
-    reply_msg = response.decode('ascii')
-    print(f"Reply from the server: {reply_msg}")
-
-s.close()
+receive_thread = threading.Thread(target=receive_messages)
+receive_thread.start()
